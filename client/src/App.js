30acebb9414d95457d88ae3d577a6d71d75824d8
ttapaninaho <tommi.tapaninaho@cerion.fi>
2018-07-06 16:57:05 +0300

    implemented map

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,92 @@
+/*global google*/
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Circle } from "react-google-maps";
+import { compose, withProps } from "recompose";
+import { GoogleMap, Marker, withGoogleMap, withScriptjs } from "react-google-maps";
+
+const { InfoBox } = require("react-google-maps/lib/components/addons/InfoBox");
+const JsonTable = require('ts-react-json-table');
+
+var box = [];
+
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyAK73U9fYt7qzTW6rrWYWqO0n-mUAAmH70&v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withScriptjs,
+  withGoogleMap
+)((props) =>
+  <GoogleMap
+    defaultZoom={13}
+    defaultCenter={{ lat: 60.4518, lng: 22.2666 }}
+  >
+    {Object.keys(props.markers).map(key => (
+        <InfoBox
+
+                  defaultPosition={new google.maps.LatLng(props.markers[key].lat, props.markers[key].lon)}
+                  options={{ closeBoxURL: ``, enableEventPropagation: true }}
+                >
+                  <div style={{ backgroundColor: props.markers[key].color, opacity: 0.75, padding: '8px', borderRadius: `50%`, border: `solid 2px darkgreen` }}>
+                    <div style={{ fontSize: `16px`, fontColor: `#08233B` }}>
+                      {props.markers[key].bikes_avail}
+                    </div>
+                  </div>
+                </InfoBox>
+    ))}
+    
+
+    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+  </GoogleMap>
+);
 
 class App extends Component {
   state = {
-    response: ''
+    response: '',
   };
 
   componentDidMount() {
     this.callApi()
       .then(res => this.setState({ response: res.express }))
       .catch(err => console.log(err));
-  }
+  };
 
   callApi = async () => {
-    const response = await fetch('/api/hello');
+    const response = await fetch('/api/bikes');
     const body = await response.json();
+    console.log(body.express);
+    console.log(typeof body.express);
+    for (var x in body.express){
+      console.log('Pyörä: ', x);
+    }
 
     if (response.status !== 200) throw Error(body.message);
 
     return body;
   };
 
+
+
   render() {
     return (
+
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">{this.state.response}</p>
+        
+        <MyMapComponent isMarkerShown
+                        markers={this.state.response} />
+
+         
+
+        <JsonTable rows={this.state.response} />
+        
       </div>
     );
   }
 }
+
 
 export default App;
--- server.js
+++ server.js
@@ -2,7 +2,47 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
-// API calls
+
+const request = require('request');
+
+var bikes = "durr";
+
+request('https://data.foli.fi/citybike/',
+	{ json: true },
+	(err, res, body) => {
+  if (err) { return console.log(err); }
+  
+  console.log(body.racks);
+  console.log(typeof body.racks);
+
+  var parsed = [];
+  for (var i in body.racks) {
+  		console.log(body.racks[i].name)
+  		var color;
+  		if(body.racks[i].bikes_avail > 2){
+  				color = 'green'
+  		} else if (body.racks[i].bikes_avail > 0) {
+  			color = 'yellow'
+  		} else {
+  			color = 'red'
+  		}
+  		
+  		parsed.push({
+  			name : body.racks[i].name,
+  			bikes_avail: body.racks[i].bikes_avail,
+  			lon: body.racks[i].lon,
+  			lat: body.racks[i].lat,
+  			color: color
+  		})
+  }
+
+  bikes = parsed;
+});
+
+app.get('/api/bikes', (req, res) => {
+	res.send({ express : bikes})
+});
+
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
@@ -15,3 +55,27 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+/*
+
+// API calls
+
+console.log("Morjens");
+
+https.get('https://data.foli.fi/citybike/', (resp) => {
+	let data = '';
+
+	resp.on('data', (chunk) => {
+		data += chunk;
+	});
+
+	resp.on('end', () => {
+			console.log(JSON.parse(data));
+	});
+
+}).on("error", (err) => {
+	console.log("Error: " + err.message);
+});
+
+*/
