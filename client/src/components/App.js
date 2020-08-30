0906680f7f7238386f64734f44fe36816ec2f65a
Tommi Tapaninaho <tommi@tapaninaho.com>
2020-08-30 21:25:15 +0300

    Implement refresh and filter

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -12,64 +12,23 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
 };
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 /*global google*/
-var react_1 = __importStar(require("react"));
+var react_1 = __importDefault(require("react"));
 require("./App.css");
 var recompose_1 = require("recompose");
 var react_google_maps_1 = require("react-google-maps");
@@ -80,63 +39,60 @@ var MyMapComponent = recompose_1.compose(recompose_1.withProps({
     containerElement: react_1.default.createElement("div", { style: { height: "500px" } }),
     mapElement: react_1.default.createElement("div", { style: { height: "100%" } }),
 }), react_google_maps_1.withScriptjs, react_google_maps_1.withGoogleMap)(function (props) {
-    return react_1.default.createElement(react_google_maps_1.GoogleMap, { defaultZoom: 13, defaultCenter: { lat: 60.4518, lng: 22.2666 } }, Object.keys(props.markers).map(function (key) { return (react_1.default.createElement(InfoBox, { defaultPosition: new google.maps.LatLng(props.markers[key].lat, props.markers[key].lon), options: { closeBoxURL: "", enableEventPropagation: true } },
-        react_1.default.createElement("div", { style: { backgroundColor: props.markers[key].color, opacity: 0.75, padding: '8px', borderRadius: "50%", border: "solid 2px darkgreen" } },
-            react_1.default.createElement("div", { style: { fontSize: "16px" } }, props.markers[key].bikes_avail)))); }));
+    return react_1.default.createElement(react_google_maps_1.GoogleMap, { defaultZoom: 13, defaultCenter: { lat: 60.4518, lng: 22.2666 } }, Object.keys(props).map(function (key) { return (react_1.default.createElement(InfoBox, { defaultPosition: new google.maps.LatLng(props[key].lat, props[key].lon), options: { closeBoxURL: "", enableEventPropagation: true } },
+        react_1.default.createElement("div", { style: {
+                backgroundColor: props[key].color,
+                opacity: 0.75,
+                padding: '8px',
+                borderRadius: "50%",
+                border: "solid 2px darkgreen"
+            } },
+            react_1.default.createElement("div", { style: { fontSize: "16px" } }, props[key].bikes_avail)))); }));
 });
 var App = /** @class */ (function (_super) {
     __extends(App, _super);
     function App() {
         var _this = _super !== null && _super.apply(this, arguments) || this;
-        _this.state = {
-            response: '',
+        _this.state = { response: [] };
+        _this.refresh = function () {
+            _this.updateRacks();
         };
-        _this.callApi = function () { return __awaiter(_this, void 0, void 0, function () {
-            var response, body, x;
-            return __generator(this, function (_a) {
-                switch (_a.label) {
-                    case 0: return [4 /*yield*/, fetch('/api/bikes')];
-                    case 1:
-                        response = _a.sent();
-                        return [4 /*yield*/, response.json()];
-                    case 2:
-                        body = _a.sent();
-                        console.log(body.express);
-                        console.log(typeof body.express);
-                        for (x in body.express) {
-                            console.log('Pyörä: ', x);
-                        }
-                        if (response.status !== 200)
-                            throw Error(body.message);
-                        return [2 /*return*/, body];
-                }
-            });
-        }); };
+        _this.filter = function () {
+            _this.setState({ response: _this.state.response.filter(function (rack) { return rack.bikes_avail > 5; }) });
+        };
         return _this;
     }
     App.prototype.componentDidMount = function () {
-        // this.callApi()
-        //   .then(res => this.setState({ response: res.express }))
-        //   .catch(err => console.log(err));
+        this.updateRacks();
+    };
+    ;
+    App.prototype.updateRacks = function () {
         var _this = this;
         fetch('/api/bikes').then(function (res) {
             res.json().then(function (json) {
-                _this.setState({ response: json.express });
+                console.log(json.express);
+                if (json.express !== null) {
+                    _this.setState({ response: json.express });
+                }
             });
-            // console.log(res.json());
-            // this.setState({response: res.json()});
-        });
-    };
-    ;
-    App.prototype.callApiSync = function () {
-        fetch('/api/bikes').then(function (response) {
-            return response.body;
         });
     };
     App.prototype.render = function () {
         return (react_1.default.createElement("div", { className: "App" },
-            react_1.default.createElement(MyMapComponent, { isMarkerShown: true, markers: this.state.response })));
+            react_1.default.createElement(MyMapComponent, __assign({}, this.state.response)),
+            react_1.default.createElement("button", { onClick: this.refresh }, "Refresh"),
+            react_1.default.createElement("button", { onClick: this.filter }, "Filter")));
     };
     return App;
-}(react_1.Component));
+}(react_1.default.PureComponent));
+var Rack = /** @class */ (function () {
+    function Rack(name, bikes_avail, lon, lat, color) {
+        this.name = name;
+        this.bikes_avail = bikes_avail;
+        this.lon = lon;
+        this.lat = lat;
+        this.color = color;
+    }
+    return Rack;
+}());
 exports.default = App;
--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,84 +1,99 @@
 /*global google*/
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
-import { compose, withProps } from 'recompose';
-import { GoogleMap, withGoogleMap, withScriptjs} from "react-google-maps";
+import {compose, withProps} from 'recompose';
+import {GoogleMap, withGoogleMap, withScriptjs} from "react-google-maps";
 
-const { InfoBox } = require("react-google-maps/lib/components/addons/InfoBox");
+const {InfoBox} = require("react-google-maps/lib/components/addons/InfoBox");
 
 
 const MyMapComponent = compose(
-  withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyAK73U9fYt7qzTW6rrWYWqO0n-mUAAmH70&v=3.exp&libraries=geometry,drawing,places",
-    loadingElement: <div style={{ height: `80%` }} />,
-    containerElement: <div style={{ height: `500px` }} />,
-    mapElement: <div style={{ height: `100%` }} />,
-  }),
-  withScriptjs,
-  withGoogleMap
+    withProps({
+        googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyAK73U9fYt7qzTW6rrWYWqO0n-mUAAmH70&v=3.exp&libraries=geometry,drawing,places",
+        loadingElement: <div style={{height: `80%`}}/>,
+        containerElement: <div style={{height: `500px`}}/>,
+        mapElement: <div style={{height: `100%`}}/>,
+    }),
+    withScriptjs,
+    withGoogleMap
 )((props: any) =>
-  <GoogleMap defaultZoom={13}
-             defaultCenter={{ lat: 60.4518, lng: 22.2666 }}>
-    {Object.keys(props.markers).map(key => (
-        <InfoBox defaultPosition={new google.maps.LatLng(props.markers[key].lat, props.markers[key].lon)}
-                  options={{ closeBoxURL: ``, enableEventPropagation: true }}>
-          <div style={{ backgroundColor: props.markers[key].color, opacity: 0.75, padding: '8px', borderRadius: `50%`, border: `solid 2px darkgreen` }}>
-            <div style={{ fontSize: `16px`}}>
-              {props.markers[key].bikes_avail}
-            </div>
-          </div>
-        </InfoBox>
-    ))}
-  </GoogleMap>
+    <GoogleMap defaultZoom={13}
+               defaultCenter={{lat: 60.4518, lng: 22.2666}}>
+        {Object.keys(props).map(key => (
+            <InfoBox defaultPosition={new google.maps.LatLng(props[key].lat, props[key].lon)}
+                     options={{closeBoxURL: ``, enableEventPropagation: true}}>
+                <div style={{
+                    backgroundColor: props[key].color,
+                    opacity: 0.75,
+                    padding: '8px',
+                    borderRadius: `50%`,
+                    border: `solid 2px darkgreen`
+                }}>
+                    <div style={{fontSize: `16px`}}>
+                        {props[key].bikes_avail}
+                    </div>
+                </div>
+            </InfoBox>
+        ))}
+    </GoogleMap>
 );
 
-class App extends Component {
-  state = {
-    response: '',
-  };
+class App extends React.PureComponent<IState> {
+    state: IState = {response: []};
 
-  componentDidMount() {
-    // this.callApi()
-    //   .then(res => this.setState({ response: res.express }))
-    //   .catch(err => console.log(err));
+    componentDidMount() {
+        this.updateRacks();
+    };
 
-    fetch('/api/bikes').then( res => {
-      res.json().then(json => {
-        this.setState({response: json.express})
-      })
-      // console.log(res.json());
-      // this.setState({response: res.json()});
-    });
-  };
+    refresh = () => {
+        this.updateRacks();
+    };
 
-  callApiSync() {
-    fetch('/api/bikes').then( response => {
-      return response.body;
-    })
-  }
+    filter = () => {
+        this.setState({response: this.state.response.filter(rack => rack.bikes_avail > 5)});
+    };
 
-  callApi = async () => {
-    const response = await fetch('/api/bikes');
-    const body = await response.json();
-    console.log(body.express);
-    console.log(typeof body.express);
-    for (let x in body.express){
-      console.log('Pyörä: ', x);
+    updateRacks() {
+        fetch('/api/bikes').then(res => {
+            res.json().then(json => {
+                console.log(json.express);
+                if (json.express !== null) {
+                    this.setState({response: json.express})
+                }
+            })
+        });
     }
 
-    if (response.status !== 200) throw Error(body.message);
+    render() {
+        return (
+            <div className="App">
+                <MyMapComponent {...this.state.response} />
+                {/*<JsonTable rows={this.state.response} />*/}
+                <button onClick={this.refresh}>Refresh</button>
+                <button onClick={this.filter}>Filter</button>
+            </div>
+        );
+    }
+}
 
-    return body;
-  };
+class Rack {
+    name: string;
+    bikes_avail: number;
+    lon: number;
+    lat: number;
+    color: string;
 
-  render() {
-    return (
-      <div className="App">
-        <MyMapComponent isMarkerShown markers={this.state.response} />
-        {/*<JsonTable rows={this.state.response} />*/}
-      </div>
-    );
-  }
+    constructor(name: string, bikes_avail: number, lon: number, lat: number, color: string) {
+        this.name = name;
+        this.bikes_avail = bikes_avail;
+        this.lon = lon;
+        this.lat = lat;
+        this.color = color;
+    }
+}
+
+interface IState {
+    response: Rack[];
 }
 
 export default App;
